fb0b715f5de4656a785dc9b9a16de6d01bf6156d
Mehmet Sinan Topal <73042487+mehmetsinann@users.noreply.github.com>
2023-06-24 15:31:11 +0300

    dotenv added and firebase keys moved to there

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,18 +1,22 @@
-// Import the functions you need from the SDKs you need
 import firebase from "firebase";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCA6KfPDB4VvTusdk8DLp64rMjkEslc-Tc",
-  authDomain: "quickform-cea9e.firebaseapp.com",
-  projectId: "quickform-cea9e",
-  storageBucket: "quickform-cea9e.appspot.com",
-  messagingSenderId: "618042911215",
-  appId: "1:618042911215:web:8b46ee2cbf1d1d43cb6e1a",
-  measurementId: "G-JE5KJN6S2N",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
