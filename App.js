71fbe857ce8e9bed110c7a3dd878c4e709f94a08
Mehmet Sinan Topal <73042487+mehmetsinann@users.noreply.github.com>
2023-07-20 10:17:23 +0300

    feat: splash screen and app icon changed

--- App.js
+++ App.js
@@ -3,11 +3,41 @@ import { StyleSheet, Text, View } from "react-native";
 import Navigation from "./src/navigation/Navigation";
 import store from "./src/redux/store";
 import { Provider } from "react-redux";
+import * as SplashScreen from "expo-splash-screen";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
-      <View style={styles.container}>
+      <View style={styles.container} onLayout={onLayoutRootView}>
         <StatusBar style="auto" />
         <Navigation />
       </View>
